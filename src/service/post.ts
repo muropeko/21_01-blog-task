7e75c14b9fa122fd72306daf358a4050a60d332f
Anastasiia Dovhoshiya <Dovgoshiya.uwo@gmail.com>
2026-01-29 23:54:54 +0200

    refactor: add fetchPosts with pagination and search support

--- src/service/post.ts
+++ src/service/post.ts
@@ -1,7 +1,38 @@
+import axios from "axios";
 import type { IPost } from "../shared/types/post.interface";
 
-export const fetchPosts = async (): Promise<IPost[]> => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (!res.ok) throw new Error("Failed to fetch posts");
-  return res.json();
+const BASE_URL = "https://jsonplaceholder.typicode.com"
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL
+});
+
+export interface FetchPostsParams {
+  _page: number
+  _limit: number
+  title_like?: string
+}
+
+export interface PostsResponse {
+  data: IPost[]
+  total: number
+}
+
+export const fetchPosts = async ({
+  _page = 1,
+  _limit = 10,
+  title_like
+}: FetchPostsParams): Promise<PostsResponse> => {
+  
+  const params = {
+    _page,
+    _limit,
+    ...(title_like?.trim() ? { title_like } : {})
+  };
+
+  const res = await axiosInstance.get<IPost[]>("/posts", { params })
+
+  const total = Number(res.headers["x-total-count"] || 0)
+
+  return { data: res.data, total }
 };
