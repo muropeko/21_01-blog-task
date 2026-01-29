a594bc0933e56d307aef59d0020ae37f16233ca4
Anastasiia Dovhoshiya <Dovgoshiya.uwo@gmail.com>
2026-01-29 15:10:55 +0200

    chore: setup TanStack Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './styles/normalize.css';
-import './styles/index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/normalize.css";
+import "./styles/index.css";
+import { QueryProvider } from "./providers/QueryProvider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryProvider>
+      <App />
+    </QueryProvider>
+  </StrictMode>
+);
